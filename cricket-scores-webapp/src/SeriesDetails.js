2d9bcba0c454b3436b4b16bb7c3b053536f9fe7d
SD614 Apps <sd614.apps@gmail.com>
2023-08-14 09:04:14 +0530

    Updated match details view on cliking series matches list

--- cricket-scores-webapp/src/SeriesDetails.js
+++ cricket-scores-webapp/src/SeriesDetails.js
@@ -1,8 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const MatchDetails = ({ matchId, closeDetails }) => {
+    const [matchInfo, setMatchInfo] = useState({});
+
+    useEffect(() => {
+        const fetchMatchInfo = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3001/match-info?matchId=${matchId}`);
+                setMatchInfo(response.data.data || {});
+            } catch (error) {
+                console.error('Error fetching match info:', error);
+            }
+        };
+
+        fetchMatchInfo();
+    }, [matchId]);
+
+    return (
+        <div className="match-details">
+            <button onClick={closeDetails}>Back to Matches</button>
+            <h3>Match Info:</h3>
+            {/* Display match details here */}
+            <div>Date: {matchInfo.date}</div>
+            <div>Teams: {matchInfo.teams}</div>
+            {/* ... Add more match details here */}
+        </div>
+    );
+};
+
 const SeriesDetails = ({ seriesId, closeDetails }) => {
     const [matches, setMatches] = useState([]);
+    const [selectedMatchId, setSelectedMatchId] = useState(null);
 
     useEffect(() => {
         const fetchMatches = async () => {
@@ -17,16 +46,25 @@ const SeriesDetails = ({ seriesId, closeDetails }) => {
         fetchMatches();
     }, [seriesId]);
 
+    if (selectedMatchId) {
+        return (
+            <MatchDetails matchId={selectedMatchId} closeDetails={() => setSelectedMatchId(null)} />
+        );
+    }
+
     return (
         <div className="series-details">
             <button onClick={closeDetails}>Close</button>
             <h3>Matches in this Series:</h3>
             <ul>
                 {matches.map((match) => (
-                    <div key={match.id} className="match-item">
+                    <li 
+                      key={match.id} 
+                      className="match-item"
+                      onClick={() => setSelectedMatchId(match.id)}
+                    >
                         [{match.date}] {match.name}
-                        {/* More match details can be added here */}
-                    </div>
+                    </li>
                 ))}
             </ul>
         </div>
