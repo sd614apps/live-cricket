b571c6c992aa6fc917395e21c0f4d87cb18bb918
SD614 Apps <sd614.apps@gmail.com>
2023-08-14 09:05:08 +0530

    Updated for player details view

--- cricket-scores-webapp/src/SearchResults.js
+++ cricket-scores-webapp/src/SearchResults.js
@@ -1,30 +1,37 @@
 import React, { useState } from 'react';
 import SeriesDetails from './SeriesDetails';
+import PlayerDetails from './PlayerDetails';
 
 const SearchResults = ({ results, searchType }) => {
     const [selectedSeries, setSelectedSeries] = useState(null);
+    const [selectedPlayer, setSelectedPlayer] = useState(null);
 
     const handleSeriesClick = (seriesId) => {
         setSelectedSeries(seriesId);
     };
 
+    const handlePlayerClick = (playerId) => {
+        setSelectedPlayer(playerId);
+    };
+
     const handleCloseDetails = () => {
         setSelectedSeries(null);
+        setSelectedPlayer(null);
     };
 
     if (selectedSeries) {
         return <SeriesDetails seriesId={selectedSeries} closeDetails={handleCloseDetails} />;
     }
 
+    if (selectedPlayer) {
+        return <PlayerDetails playerId={selectedPlayer} closeDetails={handleCloseDetails} />;
+    }
+
     if (searchType === 'series') {
         return (
             <div className="series-results">
                 {results.map((series) => (
-                    <div
-                        key={series.id}
-                        className="series-item"
-                        onClick={() => handleSeriesClick(series.id)}
-                    >
+                    <div key={series.id} className="series-item" onClick={() => handleSeriesClick(series.id)}>
                         {series.name}
                         {/* More series details can be added here */}
                     </div>
@@ -35,7 +42,7 @@ const SearchResults = ({ results, searchType }) => {
         return (
             <div className="player-results">
                 {results.map((player) => (
-                    <div key={player.id} className="player-item">
+                    <div key={player.id} className="player-item" onClick={() => handlePlayerClick(player.id)}>
                         {player.name}
                         {/* More player details can be added here */}
                     </div>
