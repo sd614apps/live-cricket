c9d808b3179e0ec9127bcf6cbb30d4598e102cd4
SD614 Apps <sd614.apps@gmail.com>
2023-08-16 23:34:17 +0530

    Updated player details view

--- cricket-scores-webapp/src/PlayerDetails.js
+++ cricket-scores-webapp/src/PlayerDetails.js
@@ -21,87 +21,82 @@ const PlayerDetails = ({ playerId, closeDetails }) => {
         return <div>Loading...</div>;
     }
 
+    const renderStatTable = (stats, title) => {
+        const matchTypes = ['test', 'odi', 't20i', 'ipl'];
+
+        return (
+            <div className="stat-table">
+                <h4>{title}</h4>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Stat</th>
+                            {matchTypes.map((matchType) => (
+                                <th key={matchType}>{matchType.toUpperCase()}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {stats.map((stat) => (
+                            <tr key={stat.stat}>
+                                <td>{stat.stat}</td>
+                                {matchTypes.map((matchType) => {
+                                    const valueObj = stat.values.find((value) => value.matchtype === matchType);
+                                    const value = valueObj ? valueObj.value : '-';
+                                    return <td key={matchType}>{value}</td>;
+                                })}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    };
+
+    const battingStats = [
+        { stat: 'Matches', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === 'm') },
+        { stat: 'Innings', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === 'inn') },
+        { stat: 'Not Outs', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === 'no') },
+        { stat: 'Runs', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === 'runs') },
+        { stat: 'Highest', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === 'hs') },
+        { stat: 'Average', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === 'avg') },
+        { stat: 'Balls Faced', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === 'bf') },
+        { stat: 'Strike Rate', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === 'sr') },
+        { stat: '100s', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === '100s') },
+        { stat: '200s', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === '200s') },
+        { stat: '50s', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === '50s') },
+        { stat: '4s', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === '4s') },
+        { stat: '6s', values: playerDetails.stats.filter((stat) => stat.fn === 'batting' && stat.stat === '6s') },
+    ];
+
+    const bowlingStats = [
+        { stat: 'Matches', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'm') },
+        { stat: 'Innings', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'inn') },
+        { stat: 'Deliveries', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'b') },
+        { stat: 'Runs Given', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'runs') },
+        { stat: 'Wickets', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'wkts') },
+        { stat: 'BBI', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'bbi') },
+        { stat: 'BBM', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'bbm') },
+        { stat: 'Economy', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'econ') },
+        { stat: 'Average', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'avg') },
+        { stat: 'Strike Rate', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === 'sr') },
+        { stat: '5 wicket hauls', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === '5w') },
+        { stat: '10 wicket hauls', values: playerDetails.stats.filter((stat) => stat.fn === 'bowling' && stat.stat === '10w') },
+    ];
+
     return (
         <div className="player-details">
             <button onClick={closeDetails} className="close-button">Close</button>
             <img src={playerDetails.playerImg} alt={playerDetails.name} />
             <h3>{playerDetails.name}</h3>
             <p>Country: {playerDetails.country}</p>
-            <p>Born: {playerDetails.dateOfBirth} in {playerDetails.placeOfBirth}</p>
+            <p>Born: {new Date(playerDetails.dateOfBirth).toLocaleDateString()} in {playerDetails.placeOfBirth}</p>
             <p>Role: {playerDetails.role}</p>
             <p>Batting Style: {playerDetails.battingStyle}</p>
             <p>Bowling Style: {playerDetails.bowlingStyle}</p>
-            {/* <p>Batting Stats:
-                <table>
-                    <tr>
-                        <th>Statistic</th>
-                        <th>Tests</th>
-                        <th>ODI</th>
-                        <th>T20</th>
-                    </tr>
-                    <tr>
-                        <td>Matches</td>
-                        <td>200</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>Innings</td>
-                        <td>329</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>Not Outs</td>
-                        <td>33</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>Runs</td>
-                        <td>15921</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>Highest Score</td>
-                        <td>248</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>Average</td>
-                        <td>53.79</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>Balls Faced</td>
-                        <td>29437</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>Strike Rate</td>
-                        <td>54.08</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>100s</td>
-                        <td>51</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                    <tr>
-                        <td>200s</td>
-                        <td>6</td>
-                        <td>Matches</td>
-                        <td>Matches</td>
-                    </tr>
-                </table>
-            </p> */}
-            {/* More player details can be added here */}
+
+            {renderStatTable(battingStats, 'Batting Stats')}
+            {renderStatTable(bowlingStats, 'Bowling Stats')}
         </div>
     );
 };
